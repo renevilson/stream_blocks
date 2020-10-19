2492dfdccbd482f8fda319e9c14ffd3461f2a324
renevilson <vildanovrinat94@gmail.com>
2020-10-19 08:10:54 +0300

    worked on the checker method

--- src/utils/check_domains.py
+++ src/utils/check_domains.py
@@ -1,6 +1,9 @@
+import requests
+
 from asyncio import sleep
 
-from src.settings import CHECK_PERIOD
+from src.models import Domain, TelegramUser
+from src.settings import CHECK_PERIOD, Session, IS_BLOCK_IN_RUSSIA_DOMAIN
 
 
 async def checker():
@@ -8,5 +11,18 @@ async def checker():
     Корутина для проверки доменов
     """
     while True:
-        print("hello")
+        session = Session()
+        domains = session.query(Domain).join(TelegramUser).with_for_update()
+
+        # TODO: надо использовать прокси сервисы, чтоб не улететь в бан
+        for domain in domains:
+            try:
+                pass
+                # response = requests.post(url=IS_BLOCK_IN_RUSSIA_DOMAIN, data={"host": domain.url})
+                # response
+        #         pass
+        #         # await sleep(100)
+            except Exception as err:
+                pass
+        session.commit()
         await sleep(CHECK_PERIOD)  # Проверка будет производиться раз в CHECK_PERIOD
